17e27f7824fd61c7fbd7737022194a72cd14a910
Chloe <144107013+Charen523@users.noreply.github.com>
2024-11-23 14:44:55 +0900

    .

---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private AudioSource bgmSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    [SerializeField] private AudioClip[] bgmClips;
+    [SerializeField] protected AudioClip[] sfxClips;
+
+    private Slider volumeSlider;
+
+    private void Start()
+    {
+        SetVolume();
+
+        if (bgmClips.Length > 0)
+        {
+            PlayBGM(0);
+        }
+    }
+
+    public void SetVolume()
+    {
+        float volume = (volumeSlider == null)? 1f : volumeSlider.value;
+        myMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+
+    public void RegisterSlider(Slider slider)
+    {
+        volumeSlider = slider;
+        volumeSlider.onValueChanged.AddListener(delegate { SetVolume(); });
+        volumeSlider.value = 1.0f;
+    }
+
+    public void PlayBGM(int clipIndex)
+    {
+        if (clipIndex < 0 || clipIndex >= bgmClips.Length)
+        {
+            Debug.LogWarning("배경음악 인덱스가 범위를 벗어났습니다!");
+            return;
+        }
+
+        bgmSource.clip = bgmClips[clipIndex];
+        bgmSource.loop = true;
+        bgmSource.Play();
+    }
+
+    public void PlaySFX(int clipIndex)
+    {
+        if (clipIndex < 0 || clipIndex >= sfxClips.Length)
+        {
+            Debug.LogWarning("효과음 인덱스가 범위를 벗어났습니다!");
+            return;
+        }
+
+        sfxSource.PlayOneShot(sfxClips[clipIndex]);
+    }
+}
