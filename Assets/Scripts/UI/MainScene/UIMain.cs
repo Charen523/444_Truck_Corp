4f958b452d7a165c2e5a520f243d398c13b27450
Chloe <144107013+Charen523@users.noreply.github.com>
2024-11-25 09:46:32 +0900

    .

--- Assets/Scripts/UI/MainScene/UIMain.cs
+++ Assets/Scripts/UI/MainScene/UIMain.cs
@@ -172,8 +172,9 @@ public class UIMain : MonoBehaviour
         {
             if (h == eHeroState.TRAINING) trainCount++;
         }
+        trainCount *= 20;
         if (trainCount != 0) GameManager.Instance.OnHeroTrainingGoldEvent?.Invoke(trainCount);
-        GameManager.Instance.OnGoldChangeEvent(-trainCount * 20);
+        GameManager.Instance.OnGoldChangeEvent(-trainCount);
 
         GameManager.Instance.OnDayChangeEvent(1);
 
